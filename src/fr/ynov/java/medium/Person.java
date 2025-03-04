4074b31f45773e533cc89863fdcfd3db1722430d
Delprat Loïc <loic.delprat@ynov.com>
2025-03-04 17:21:37 +0100

    feat(Enum): made first part of Method an Enum

--- src/fr/ynov/java/medium/Person.java
+++ src/fr/ynov/java/medium/Person.java
@@ -1,5 +1,7 @@
 package fr.ynov.java.medium;
 
+import java.util.Date;
+
 enum Nationality {
     ENGLISH,
     FRENCH,
@@ -13,5 +15,26 @@ public class Person {
     float weight;
     Nationality nationality;
 
+    public Person(String name, int age, String gender, float height, float weight, Nationality nationality) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight;
+        this.nationality = nationality;
+    }
 
+    public void showAttributes() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+        System.out.println(this.gender);
+        System.out.println(this.height);
+        System.out.println(this.weight);
+        System.out.println(this.nationality);
+    }
+
+    public static void main(String[] args) {
+        Person oui = new Person("lolo",18,"trans",1.79f,68.9f,Nationality.FRENCH);
+        oui.showAttributes();
+    }
 }
