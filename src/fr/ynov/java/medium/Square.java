29206cbcc412038829d7e8c6c6e24ede10e66f47
Delprat Loïc <loic.delprat@ynov.com>
2025-03-04 16:56:52 +0100

    feat(Methods): added Square class and method

---
+++ src/fr/ynov/java/medium/Square.java
@@ -0,0 +1,11 @@
+package fr.ynov.java.medium;
+
+public class Square {
+    public static void main(String[] args) {
+        System.out.println(square(2));
+    }
+
+    public static int square(int num) {
+        return num * num;
+    }
+}
